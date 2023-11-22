fa8207ef587120dcb6e9bc1948b313c131b83312
SmirnovaAnastasia <sm.anastasiya2010@yandex.ru>
2023-11-22 18:06:42 +0700

    Add edit second lab

--- Lab2/main2.c
+++ Lab2/main2.c
@@ -13,8 +13,15 @@ int main(int argc, char const *argv[])
 		scanf("%d", &arr[i]);
 	}
 
+	for (int i = 0; i <= n/2; ++i)
+	{
+		int swap = arr[i];
+		arr[i] = arr[n - 1 - i];
+		arr[n - 1 - i] = swap;
+	}
+
 	printf("Reverse array:\n");
-	for (int i = n - 1; i >= 0 ; --i)
+	for (int i = 0; i < n; ++i)
 	{
 		printf("%d ", arr[i]);
 	}
